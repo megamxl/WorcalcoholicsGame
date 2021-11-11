c01a8f45da6edf0847175cfc95dc67f307d43ea1
chrop01 <91630967+chrop01@users.noreply.github.com>
2021-11-11 22:11:25 +0100

    Enemy class created, Handler is excluded because not implemented yet.

---
+++ src/Woralcoholics/game/Enemy.java
@@ -0,0 +1,86 @@
+package Woralcoholics.game;
+
+import java.awt.*;
+import java.util.Random;
+
+//Part 9
+//Handler -> not implemented yet
+
+public class Enemy extends GameObject {
+
+    //Handler is for collision detection
+    //private Handler handler;
+    Random r = new Random();
+    int choose = 0;
+    int hp = 100;
+    int low = -4;
+    int high = 4;
+
+    public Enemy(int x, int y, ID id) {
+        super(x, y, id);
+        //this.handler = handler;
+    }
+
+    public void update() {
+        x += velX;
+        y += velY;
+
+        choose = r.nextInt(10);
+
+        for (int i = 0; i < handler.object.size(); i++) {
+
+            GameObject tmpObject = handler.object.get(i);
+
+            //if it's coliding with a block
+            if (tmpObject.getId() == ID.Block) {
+                if (getBoundsAround().intersects(tmpObject.getBounds())) {
+                   // if it coliding with wall it goes in the opposite direction
+                    x += (velX * 5) * -1;                                       // maybe improve these values(velx*5,velxy*= -1)
+                    y += (velX * 5) * -1;
+                    velX *= -1;
+                    velY *= -1;
+                } else if (choose == 0) {
+                    //moving between 4 and -4 per iterate
+                    velX = r.nextInt(high - low) + low;
+                    velY = r.nextInt(high - low) + low;
+                }
+
+
+            }
+
+            //if our bullet is coliding with the enemy hp get's -50
+            if (tmpObject.getId() == ID.Bullet) {
+                if (getBounds().intersects(tmpObject.getBounds())) {
+                    hp -= 50;
+                    handler.removeObject(tmpObject);
+                }
+            }
+
+        }
+
+        if (hp <= 0) {
+            handler.removeObject(this);
+        }
+    }
+
+    //Enemy is displaying in this Color for now
+    public void render(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, 32, 32);
+
+        /* -> Just for testing Bounding Boxes around the Enemys
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(Color.GREEN);
+        g2d.draw(getBoundsBig());
+         */
+
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 32, 32);
+    }
+
+    public Rectangle getBoundsAround() {
+        return new Rectangle(x - 16, y - 16, 64, 64);
+    }
+}
