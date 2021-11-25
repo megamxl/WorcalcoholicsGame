3504bdcf008b594c7be062eb86a2ce22cc8ec356
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2021-11-26 00:45:40 +0100

    ah yeah, imported some dumb shit, another push

--- src/Woralcoholics/game/Game.java
+++ src/Woralcoholics/game/Game.java
@@ -4,8 +4,6 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
