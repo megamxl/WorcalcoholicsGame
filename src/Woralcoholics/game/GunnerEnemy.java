a0c757306e11f0e8370a21463b68dd86230ba695
Dr. Schluk <lukas.schelepet@aon.at>
2021-12-29 23:30:58 +0100

    - Gunner now chills for 3 seconds before shooting - Commented GunnerEnemy.java
    TODO
    - Add Screen-Images
    - Add LoS method (so it only shoots when nothing is between Gunner and Player)
    - Add GunnerState for walking around walls
    - (optional?) Always render Player above Bullets
    - (optional) Changing mouse cursor to crosshair while in Window

--- src/Woralcoholics/game/GunnerEnemy.java
+++ src/Woralcoholics/game/GunnerEnemy.java
@@ -10,41 +10,43 @@ public class GunnerEnemy extends Enemy {
 
     GameManager handler;
 
-    private final BufferedImage gunner_enemy_img;
+    private final BufferedImage gunner_enemy_img;       //Sprite of Gunner Enemy
 
-    private float minDistanceToPlayer = 250;
+    private float minDistanceToPlayer = 250;        //the minimal and maximum distances the Gunner should be away from the player
     private float maxDistanceToPlayer = 500;
     private double distanceToPlayer;
-    private double alpha;
+    private double alpha;       //angle that is needed for calculating the bullet path
 
     private final int checkFreeDistance = 32+50;
 
-    public static float movementSpeed = 5;
+    public static float movementSpeed = 5;  //Movement-Speed of the Gunner
 
-    protected double px = 0, py = 0;
-    protected double gx, gy;
+    private double px = 0, py = 0;    //Player Coordinates
+    private double gx, gy;            //Gunner Coordinates
 
-    private double wait = 0;
-    private final double shootDel = 1500;
+    private double wait = 3000 + System.currentTimeMillis();        //initial wait time for shooting Bullets
+    private final double shootDel = 1500;   //Interval time at which the Gunner should shoot
 
-    private enum state {
+    private enum state {    //All different states the Gunner can be in
         TOO_CLOSE,
         TOO_FAR,
         NEAR_WALL,
         STAY
     }
-
     private state gunnerState;
 
 
-    public GunnerEnemy(int x, int y, ID id, GameManager manager, Animations an, Score score) {
+    public GunnerEnemy(int x, int y, ID id, GameManager manager, Animations an, Score score) {  //Constructor for Gunner Enemies
         super(x, y, id, manager, an, score);
         handler = manager;
-        gx = getX();
+        gx = getX();    //get Position of Gunner
         gy = getY();
-        gunner_enemy_img = an.getImage(1,5,64,64);
+        gunner_enemy_img = an.getImage(1,5,64,64);  //Get the Sprite for the Gunner from Spritesheet
     }
 
+    /***
+     * Function to calculate the Distance to the player, and act accordingly
+     */
     private void calcDistanceToPlayer() {
         for(int i = 0; i < handler.object.size(); i++) {
             GameObject temp = handler.object.get(i);
@@ -68,30 +70,36 @@ public class GunnerEnemy extends Enemy {
             gunnerState = state.STAY;
     }
 
+    /***
+     * How the Gunner should act depending on its state
+     */
     public void behaviour() {
         switch(gunnerState) {
-            case TOO_FAR -> {
+            case TOO_FAR -> {   //-> move closer to the player
                 velX = (float) (Math.cos(alpha) * movementSpeed);
                 velY = (float) (Math.sin(alpha) * movementSpeed);
             }
             case TOO_CLOSE -> {
-                alpha += Math.PI;
+                alpha += Math.PI;   //-> move away from the player
                 velX = (float) (Math.cos(alpha) * movementSpeed);
                 velY = (float) (Math.sin(alpha) * movementSpeed);
             }
             case NEAR_WALL -> {
                 //alpha += checkIfFree();
             }
-            case STAY -> velX = velY = 0;
+            case STAY -> velX = velY = 0;   //stay still
         }
         //System.out.println(gunnerState);
     }
 
+    /***
+     * Shoot a bullet
+     */
     public void shoot() {
         gx += 16;
         gy += 16;
-        EnemyBullet temp = new EnemyBullet((int) gx-4, (int) gy-4, ID.EnemyBullet, handler, an);
-        temp.direction(px, py, gx, gy);
+        EnemyBullet temp = new EnemyBullet((int) gx-4, (int) gy-4, ID.EnemyBullet, handler, an);    //Create a new bullet, that can harm the player
+        temp.direction(px, py, gx, gy);     //Set the direction
         handler.addObject(temp);
         playSoundGunnerEnemy();
     }
@@ -121,26 +129,29 @@ public class GunnerEnemy extends Enemy {
         }
     }
 
-    public boolean los(GameObject o) {
-        return true;
-    }
-
+    /***
+     * Update function of GunnerEnemy
+     */
     public void update() {
         gx = getX();
         gy = getY();
         calcDistanceToPlayer();
         behaviour();
         move();
-        collision();
+        collision();    //Check if it's colliding with a Bullet or Block (Enemy.collision())
         //checkIfFree();
+        //Shoot after a delay
         double now = System.currentTimeMillis();
         if(now > wait) {
             shoot();
             wait = now + shootDel;
         }
-        isDead();
+        isDead();   //Check if he is dead (Enemy.isDead())
     }
 
+    /***
+     * Render function of GunnerEnemy
+     */
     public void render(Graphics g) {
         /*g.setColor(Color.MAGENTA);
         g.fillRect((int)x +4, (int)y +2, 52, 60);
@@ -161,6 +172,9 @@ public class GunnerEnemy extends Enemy {
         return new Rectangle((int)gx, (int)gy, 50, 50);
     }
 
+    /***
+     * Some getter and setter functions
+     */
     public float getMinDistanceToPlayer() {
         return minDistanceToPlayer;
     }
