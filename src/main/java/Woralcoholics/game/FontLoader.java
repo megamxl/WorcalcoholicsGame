a9becaad7dd0a7e415f9000fca49837cb55360ae
gustavopdzwtFHCAMP <gustavo.podzuweit@stud.fh-campuswien.ac.at>
2022-01-24 22:36:18 +0100

    added weapon images to spritesheet and added new font to files

--- src/main/java/Woralcoholics/game/FontLoader.java
+++ src/main/java/Woralcoholics/game/FontLoader.java
@@ -13,6 +13,7 @@ public class FontLoader {
             try {
                 // get the Font files as Java object fonts
                 Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("Resource/Fonts/Masked Hero.ttf")).deriveFont(12f);
+                //Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("Resource/Fonts/PressStart2P-Regular.ttf")).deriveFont(12f);
                 Font customFont1 = Font.createFont(Font.TRUETYPE_FONT, new File("Resource/Fonts/DebugFreeTrial-MVdYB.otf")).deriveFont(12f);
                 Font customFont2 = Font.createFont(Font.TRUETYPE_FONT, new File("Resource/Fonts/Cyberpunk-Regular.ttf")).deriveFont(12f);
 
