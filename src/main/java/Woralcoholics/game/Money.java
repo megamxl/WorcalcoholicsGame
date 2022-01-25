7c318fa0988143a42b1f19cf3cdd18e2948ed67e
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2022-01-25 21:20:35 +0100

    Creditscreen, tell me what you would want to be changed

---
+++ src/main/java/Woralcoholics/game/Money.java
@@ -0,0 +1,25 @@
+package Woralcoholics.game;
+
+public class Money {
+    private int money;
+
+    public Money(){
+        this.money = 0;
+    }
+
+    public void setMoney(int amount){
+        this.money = amount;
+    }
+
+    public void addMoney(int amount){
+        this.money += amount;
+    }
+
+    public int getMoney(){
+        return this.money;
+    }
+
+    public void purchase(int amount){
+        this.money -= amount;
+    }
+}
